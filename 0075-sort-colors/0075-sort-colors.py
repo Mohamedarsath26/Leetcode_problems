4472f4368c93e8fec31e392f2eb20afcb7f22b6c
Mohamed Arsath M <138310244+Mohamedarsath26@users.noreply.github.com>
2024-09-21 17:53:44 +0530

    Time: 38 ms (52.82%), Space: 16.8 MB (23.61%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,5 +3,25 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        nums[:] = sorted(nums)
-        
+        #nums[:] = sorted(nums)  Bruteforece solution
+
+        cnt = 0
+        cnt_1 = 0
+        cnt_2 = 0
+
+        for i in range(len(nums)):
+            if (nums[i]==0):
+                cnt+=1
+            elif (nums[i] == 1):
+                cnt_1+=1
+            elif (nums[i]==2):
+                cnt_2+=1
+        for i in range(cnt): #0 to 2
+            print(i)
+            nums[i] = 0
+        for i in range(cnt,cnt+cnt_1): # 2 to 4
+            print(i)
+            nums[i] = 1
+        for i in range(cnt+cnt_1,cnt_1+cnt_2+cnt): # 4 to 6
+            print(i)
+            nums[i] = 2
