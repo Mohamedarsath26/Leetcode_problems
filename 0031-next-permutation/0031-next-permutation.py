e35ba82db7428c4fd22c8e26a28967b999f3475e
Mohamed Arsath M <138310244+Mohamedarsath26@users.noreply.github.com>
2024-09-26 23:27:47 +0530

    Time: 35 ms (92.98%), Space: 16.3 MB (96.59%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -1,22 +1,29 @@
+from typing import List
+
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
+        This function rearranges numbers into the next lexicographical permutation.
         """
         ind = -1
         n = len(nums)
-        for i in range(n-2,-1,-1):
-            if nums[i] < nums[i+1]:
+        
+        # Step 1: Find the first decreasing element from the end
+        for i in range(n - 2, -1, -1):
+            if nums[i] < nums[i + 1]:
                 ind = i
                 break
         
+        # Step 2: If no such element exists, reverse the entire array
         if ind == -1:
             nums[:] = sorted(nums)
         else:
-            for i in range(n-1,-1,-1):
+            # Step 3: Find the element just larger than nums[ind] and swap them
+            for i in range(n - 1, ind, -1):
                 if nums[i] > nums[ind]:
-                    nums[i],nums[ind] = nums[ind],nums[i]
+                    nums[i], nums[ind] = nums[ind], nums[i]
                     break
             
+            # Step 4: Reverse the part of the array after the swap index (ind+1 to end)
             nums[ind + 1:] = sorted(nums[ind + 1:])
-                
