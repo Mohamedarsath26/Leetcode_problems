2921fc8304323efe8cbab8db334dd330fe28a8b5
Mohamed Arsath M <138310244+Mohamedarsath26@users.noreply.github.com>
2024-09-29 12:22:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -4,20 +4,52 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         # """
 
-        col = set()
-        row = set()
+        # col = set()
+        # row = set()
+        # for i in range(len(matrix)):
+        #     for j in range(len(matrix[i])):
+        #         if matrix[i][j] == 0:
+        #             col.add(j) 
+        #             row.add(i)
+                    
+        # for i in row:
+        #     matrix[i] = [0]*len(matrix[i])
+
+        # for i in range(len(matrix)):
+        #     for j in col:
+        #         matrix[i][j] = 0  
+
+        # return matrix
+        col0 = 1
+        # step 1: Traverse the matrix and
+        # mark 1st row & col accordingly:
         for i in range(len(matrix)):
             for j in range(len(matrix[i])):
                 if matrix[i][j] == 0:
-                    col.add(j) 
-                    row.add(i)
-                    
-        for i in row:
-            matrix[i] = [0]*len(matrix[i])
+                    # mark i-th row:
+                    matrix[i][0] = 0
 
-        for i in range(len(matrix)):
-            for j in col:
-                matrix[i][j] = 0  
+                    # mark j-th column:
+                    if j != 0:
+                        matrix[0][j] = 0
+                    else:
+                        col0 = 0
+
+        # Step 2: Mark with 0 from (1,1) to (n-1, m-1):
+        for i in range(1, len(matrix)):
+            for j in range(1, len(matrix[i])):
+                if matrix[i][j] != 0:
+                    # check for col & row:
+                    if matrix[i][0] == 0 or matrix[0][j] == 0:
+                        matrix[i][j] = 0
+
+        #step 3: Finally mark the 1st col & then 1st row:
+        if matrix[0][0] == 0:
+            for j in range(len(matrix[1])):
+                matrix[0][j] = 0
+        if col0 == 0:
+            for i in range(len(matrix)):
+                matrix[i][0] = 0
 
         return matrix
 
