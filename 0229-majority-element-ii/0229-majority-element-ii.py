631e1542ee1746b6e1108dc93e62a56cb1f9dfd5
Mohamed Arsath M <138310244+Mohamedarsath26@users.noreply.github.com>
2024-10-12 23:46:00 +0530

    Time: 100 ms (48.18%), Space: 17.9 MB (30.1%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,19 +1,43 @@
+from typing import List
+
 class Solution:
-    def majorityElement(self, nums: List[int]) -> int:
-        hash = {}
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        cnt_1 = 0
+        cnt_2 = 0
+        ele_1 = None
+        ele_2 = None
         ls = []
-        for num in nums:
-            if num in hash:
-                hash[num] += 1
-            else:
-                hash[num] = 1
-        print(hash)
-        max_val = (len(nums)//3)
-        print(max_val)
+        n_times = len(nums) // 3
         
-        for key,val in hash.items():
-            if val > max_val:
-                ls.append(key)
+        # First pass: Find two potential candidates
+        for num in nums:
+            if ele_1 == num:
+                cnt_1 += 1
+            elif ele_2 == num:
+                cnt_2 += 1
+            elif cnt_1 == 0:
+                ele_1 = num
+                cnt_1 = 1
+            elif cnt_2 == 0:
+                ele_2 = num
+                cnt_2 = 1
+            else:
+                cnt_1 -= 1
+                cnt_2 -= 1
+        
+        # Second pass: Count the occurrences of the two candidates
+        cnt_1 = 0
+        cnt_2 = 0
+        for num in nums:
+            if num == ele_1:
+                cnt_1 += 1
+            elif num == ele_2:
+                cnt_2 += 1
+
+        # Check if the candidates appear more than n//3 times
+        if cnt_1 > n_times:
+            ls.append(ele_1)
+        if cnt_2 > n_times:
+            ls.append(ele_2)
 
         return ls
-                
