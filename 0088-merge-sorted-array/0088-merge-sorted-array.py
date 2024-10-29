5fc0f63ad9e24db6199ede1c4c0265a88596dc28
Mohamed Arsath M <138310244+Mohamedarsath26@users.noreply.github.com>
2024-10-29 14:53:13 +0530

    Time: 0 ms (100%), Space: 16.7 MB (7.73%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,30 +3,51 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        left = 0
-        right = 0
-        index = 0
+        # left = 0
+        # right = 0
+        # index = 0
 
         arr1 = nums1[:m]
+        print(arr1)
         arr2 = nums2[:n]
+        print(arr2)
 
-        while left < m and right < n:
-            if arr1[left] < arr2[right]:
-                nums1[index] = arr1[left]
-                left+=1
+        # while left < m and right < n:
+        #     if arr1[left] < arr2[right]:
+        #         nums1[index] = arr1[left]
+        #         left+=1
 
-            else:
-                nums1[index] = arr2[right]
-                right+=1
+        #     else:
+        #         nums1[index] = arr2[right]
+        #         right+=1
 
-            index+=1
+        #     index+=1
         
-        while(left < m):
-            nums1[index] = arr1[left]
-            left+=1
-            index+=1
+        # while(left < m):
+        #     nums1[index] = arr1[left]
+        #     left+=1
+        #     index+=1
   
-        while(right < n):
-            nums1[index] = arr2[right]
-            right+=1
-            index+=1
+        # while(right < n):
+        #     nums1[index] = arr2[right]
+        #     right+=1
+        #     index+=1
+
+        left = m - 1
+        right = 0
+
+        # Swap the elements until arr1[left] is smaller than arr2[right]:
+        while left >= 0 and right < n:
+            if arr1[left] > arr2[right]:
+                arr1[left], arr2[right] = arr2[right], arr1[left]
+                left -= 1
+                right += 1
+            else:
+                break
+
+        # Sort arr1[] and arr2[] individually:
+        arr1.sort()
+        arr2.sort()
+
+        nums1[:] = arr1 + arr2
+
