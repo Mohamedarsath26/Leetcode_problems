f3d933f4b530a5222ab85a393a92e8ced7943117
Mohamed Arsath M <138310244+Mohamedarsath26@users.noreply.github.com>
2024-10-29 13:48:35 +0530

    Time: 0 ms (100%), Space: 16.6 MB (30.93%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -1,33 +1,32 @@
-from typing import List
-
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        
-        arr1 = nums1[:m]  
-        arr2 = nums2[:n]  
+        left = 0
+        right = 0
+        index = 0
 
-        i, j = 0, 0
-        k = 0  
-        
+        arr1 = nums1[:m]
+        arr2 = nums2[:n]
 
-        while i < len(arr1) and j < len(arr2):
-            if arr1[i] < arr2[j]:
-                nums1[k] = arr1[i]
-                i += 1
+        while left < m and right < n:
+            if arr1[left] < arr2[right]:
+                nums1[index] = arr1[left]
+                left+=1
+
             else:
-                nums1[k] = arr2[j]
-                j += 1
-            k += 1
+                nums1[index] = arr2[right]
+                right+=1
 
-        while i < len(arr1):
-            nums1[k] = arr1[i]
-            i += 1
-            k += 1
-
-        while j < len(arr2):
-            nums1[k] = arr2[j]
-            j += 1
-            k += 1
+            index+=1
+        
+        while(left < m):
+            nums1[index] = arr1[left]
+            left+=1
+            index+=1
+  
+        while(right < n):
+            nums1[index] = arr2[right]
+            right+=1
+            index+=1
