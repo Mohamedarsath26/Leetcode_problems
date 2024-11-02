5f8b32244b124b70fe2ab50679d20f8c82ff0559
Mohamed Arsath M <138310244+Mohamedarsath26@users.noreply.github.com>
2024-11-02 15:17:23 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,27 +1,37 @@
 class Solution:
+    def lower_bound(self,a,n,x):
+        low = 0
+        high = n-1
+        ans = n
+        while (low<=high):
+            mid = (low+high)//2
+            if a[mid] >= x:
+                ans = mid
+                high = mid - 1
+            else:
+                low = mid +1
+        return ans
+    def upper_bound(self,a,n,x):
+        low = 0
+        high = n-1
+        ans = n
+        while (low<high):
+            mid = (low+high)//2
+            if a[mid] >= x:
+                ans = mid
+                high = mid - 1
+            else:
+                low = mid +1
+        return ans
+
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        def bin_tree(nums,target,boo):
-            low = 0
-            high = len(nums) - 1
-            idx = -1
-            while low<=high:
-                mid = (low+high)//2
+        n = len(nums)
 
-                if nums[mid] > target:
-                    high = mid-1
-                elif nums[mid] < target:
-                    low = mid+1
-                else:
-                    idx = mid
-                    if boo:
-                        high = mid - 1
-                    else:
-                        low = mid + 1
+        lb = self.lower_bound(nums,n,target)
+        print(lb)
+        if (lb == n or nums[lb] != target):
+            return [-1,-1]
+        else:
+            ub = self.upper_bound(nums,n,target)
 
-            return idx
-
-        left = bin_tree(nums,target,True)
-        right = bin_tree(nums,target,False)
-
-        return [left,right]
-                    
+        return [lb,ub]
