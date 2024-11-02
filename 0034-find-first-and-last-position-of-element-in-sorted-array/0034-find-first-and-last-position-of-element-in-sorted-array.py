a5866b1aa844ea3e464af5c9c45ad5180b11015d
Mohamed Arsath M <138310244+Mohamedarsath26@users.noreply.github.com>
2024-11-02 15:28:01 +0530

    Time: 0 ms (100%), Space: 18.2 MB (14.97%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,37 +1,70 @@
 class Solution:
+    # def lower_bound(self,a,n,x):
+    #     low = 0
+    #     high = n-1
+    #     ans = n
+    #     while (low<=high):
+    #         mid = (low+high)//2
+    #         if a[mid] >= x:
+    #             ans = mid
+    #             high = mid - 1
+    #         else:
+    #             low = mid +1
+    #     return ans
+
+    # def upper_bound(self,a,n,x):
+    #     low = 0
+    #     high = n-1
+    #     ans = n
+    #     while (low<=high):
+    #         mid = (low+high)//2
+    #         if a[mid] > x:
+    #             ans = mid
+    #             high = mid - 1
+    #         else:
+    #             low = mid +1
+    #     return ans
+
     def lower_bound(self,a,n,x):
         low = 0
         high = n-1
         ans = n
         while (low<=high):
             mid = (low+high)//2
-            if a[mid] >= x:
+            if a[mid] == x:
                 ans = mid
                 high = mid - 1
+            elif a[mid] < x:
+                low = mid + 1
             else:
-                low = mid +1
+                high = mid - 1
         return ans
+
     def upper_bound(self,a,n,x):
         low = 0
         high = n-1
         ans = n
         while (low<=high):
             mid = (low+high)//2
-            if a[mid] > x:
+            if a[mid] == x:
                 ans = mid
-                high = mid - 1
-            else:
+                low = mid + 1
+            elif a[mid] < x:
                 low = mid +1
+            else:
+                high = mid - 1
         return ans
+
 
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         n = len(nums)
 
-        lb = self.lower_bound(nums,n,target)
+        lb = self.lower_bound(nums,n,target) # gives the first occurence
         print(lb)
         if (lb == n or nums[lb] != target):
             return [-1,-1]
         else:
-            ub = self.upper_bound(nums,n,target)
+            ub = self.upper_bound(nums,n,target)  # gives the last occurence
 
-        return [lb,ub-1]
+        return [lb,ub]
+
