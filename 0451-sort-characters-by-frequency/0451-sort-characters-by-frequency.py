9a7a7850c2cefe7d6ba33cbf54e9ae465899215a
Mohamed Arsath M <138310244+Mohamedarsath26@users.noreply.github.com>
2025-04-20 23:14:57 +0530

    Time: 12 ms (37.91%), Space: 18.9 MB (81.21%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -2,10 +2,18 @@ class Solution:
     def frequencySort(self, s: str) -> str:
 
         list_str = []
+        
+        dict_char = {}
 
-        sorted_char = Counter(s).most_common() ## returns the tuple of char and count
+        for char in s:
+            if char in dict_char:
+                dict_char[char] += 1
+            else:
+                dict_char[char] = 1
+        
+        des_char = dict(sorted(dict_char.items() ,key = lambda item: item[1],reverse=True))
 
-        for char,count in sorted_char:
+        for char,count in des_char.items():
             list_str.append(char*count)
         return ''.join(list_str)
 
