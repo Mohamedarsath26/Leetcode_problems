54d5799ece926abe668555a4c5cb844a0098ba08
Mohamed Arsath M <138310244+Mohamedarsath26@users.noreply.github.com>
2025-05-04 19:15:13 +0530

    Time: 4 ms (2.23%), Space: 17.9 MB (12.88%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -12,26 +12,21 @@ class Solution:
         cnt = 0
         prev = None
         temp = head
-
-        # First pass: count total nodes
         while temp:
             cnt += 1
             temp = temp.next
 
-        # If the node to delete is the head
         if cnt == n:
             return head.next
         
         temp = head
-
-        # Second pass: move to (cnt - n)th node
+        
         while temp:
             if cnt == n:
-                # prev is guaranteed to not be None here
                 prev.next = prev.next.next
-                  # Node deleted, exit loop
             prev = temp
             temp = temp.next
-            cnt -= 1
+            cnt-=1
         
         return head
+        
