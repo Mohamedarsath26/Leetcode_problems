9a095cbcf618c0d1d532bdac92eaf5da184a5b8d
Mohamed Arsath M <138310244+Mohamedarsath26@users.noreply.github.com>
2025-05-05 19:28:34 +0530

    Time: 202 ms (29.32%), Space: 32.9 MB (69.1%) - LeetHub

--- 0148-sort-list/0148-sort-list.py
+++ 0148-sort-list/0148-sort-list.py
@@ -1,21 +1,64 @@
+# Definition for singly-linked list.
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
 
 class Solution:
-    def sortList(self, head: ListNode) -> ListNode:
-        heap = []
-        curr = head
+    def findmiddle(self,head):
+        slow = head
+        fast = head.next
 
-        while curr:
-            heapq.heappush(heap,curr.val)
-            curr = curr.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
 
-        curr= head
-        while curr:
-            pop = heapq.heappop(heap)
-            curr.val = pop
-            curr = curr.next
+        return slow
+
+    def mergeTwolist(self,l1,l2):
+        dummy = ListNode(-1)
+        temp = dummy
+
+        while l1 and l2:
+            if l1.val < l2.val:
+                temp.next = l1
+                l1 = l1.next
+                temp = temp.next
+            else:
+                temp.next = l2
+                l2 = l2.next
+                temp = temp.next
+            
+        if l1:
+            temp.next = l1
+            l1 = l1.next
+            temp = temp.next
+
+        if l2:
+            temp.next = l2
+            l2 = l2.next
+            temp = temp.next
         
-        return head
+        return dummy.next
+
+
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
+        if head == None or head.next == None:
+            return head
+        
+        middle = self.findmiddle(head)
+        right = middle.next
+        middle.next = None
+        left = head
+
+        left = self.sortList(left)
+        right = self.sortList(right)
+
+        return self.mergeTwolist(left,right)
+
+
+
+
+
+        
